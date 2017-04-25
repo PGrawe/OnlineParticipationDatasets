5b091ddfda6ac39970aa9c2cc31115351f2091a4
Matthias Liebeck <Liebeck@users.noreply.github.com>
2017-04-25 15:56:53 +0200

    Add Dockerfile

---
+++ Dockerfile.py
@@ -0,0 +1,20 @@
+FROM liebeck/miniconder3
+
+RUN apt-get update && \
+	apt-get install -y build-essential libxml2-dev libxslt-dev python-matplotlib libsm6 libxrender1 libfontconfig1 libicu-dev python-dev python-qt4 && \
+	apt-get clean
+
+# install packages with conda
+RUN conda install -y \
+  pip
+
+RUN conda remove mkl mkl-service
+
+RUN sudo echo "Europe/Berlin " > /etc/timezone
+RUN sudo dpkg-reconfigure -f noninteractive tzdata
+
+
+WORKDIR /var/www
+ADD . .
+RUN pip install -r requirements.txt
+RUN pip install -e .
