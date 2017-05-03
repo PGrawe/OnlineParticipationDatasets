f6184cf03dd12ad9f3131d43a3461c4fa376a81e
PGrawe <PGrawe@users.noreply.github.com>
2017-05-03 20:26:05 +0200

    Gets the first title of threads on the first page

--- onlineparticipation/crawlers/bonn2011.py
+++ onlineparticipation/crawlers/bonn2011.py
@@ -1,5 +1,16 @@
 import scrapy
 
 class Bonn2011Spider(scrapy.Spider):
-    name = "bonn2011_spider"
+    name = "bonn2011"
     start_urls = ['http://bonn-packts-an-2011.de/www.bonn-packts-an.de/dito/forumc0d2.html']
+    # TODO: parse the list_pages for next page/last -> maybe in start_requests
+    #response.css('div.list_pages a').extract()
+    def parse(self, response):
+        # div.vorschlag buergervorschlag doesn't work -> XPath
+        for thread in response.css('div.vorschlaege'):
+            # TODO: Follow each link
+            yield {
+                # TODO: Parse id
+                'id' : thread.css('h2::text').extract_first()
+                #'title' :
+            }
