1a013afa6fe03ddf64496f976d61757e1728499d
PGrawe <PGrawe@users.noreply.github.com>
2017-05-04 15:20:26 +0200

    Scrape title, id and link on 1. Page

--- onlineparticipation/crawlers/bonn2011.py
+++ onlineparticipation/crawlers/bonn2011.py
@@ -6,11 +6,11 @@ class Bonn2011Spider(scrapy.Spider):
     # TODO: parse the list_pages for next page/last -> maybe in start_requests
     #response.css('div.list_pages a').extract()
     def parse(self, response):
-        # div.vorschlag buergervorschlag doesn't work -> XPath
-        for thread in response.css('div.vorschlaege'):
+        for thread in response.css('div.vorschlag.buergervorschlag'):
             # TODO: Follow each link
             yield {
                 # TODO: Parse id
-                'id' : thread.css('h2::text').extract()
-                #'title' :
+                'id' : thread.css('h2::text').extract_first(),
+                'title' : thread.css('div.col_01 h3 a::text').extract_first(),
+                'link' : thread.css('div.col_01 h3 a::attr(href)').extract_first()
             }
