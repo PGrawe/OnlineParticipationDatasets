3672d178ac4f9a3f9308acf1e43e9eea663fe30a
PGrawe <PGrawe@users.noreply.github.com>
2017-06-05 16:45:54 +0200

    Create path if it doesnt exists

--- OnlineParticipationDataset/pipelines.py
+++ OnlineParticipationDataset/pipelines.py
@@ -5,9 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import json
+import json,os
 from datetime import datetime
 from scrapy.exporters import JsonLinesItemExporter
+
+path = "downloads"
 
 
 class OnlineparticipationdatasetPipeline(object):
@@ -15,11 +17,11 @@ class OnlineparticipationdatasetPipeline(object):
         return item
 
 
-
-
 class JsonWriterPipeline(object):
 
     def open_spider(self, spider):
+        if not os.path.isdir(path):
+            os.makedirs(path)
         self.file = open("downloads/items_"+spider.name+".json", 'wb')
         self.exporter = JsonLinesItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
         self.exporter.start_exporting()
