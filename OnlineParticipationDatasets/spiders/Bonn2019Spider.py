eedae8070304e9dc932ea066486b8ea902d49daa
PGrawe <PGrawe@users.noreply.github.com>
2018-05-16 17:27:14 +0200

    Changed structure for bonn

--- OnlineParticipationDatasets/spiders/Bonn2019Spider.py
+++ OnlineParticipationDatasets/spiders/Bonn2019Spider.py
@@ -1,12 +1,12 @@
 import scrapy
 from OnlineParticipationDatasets import items
-from OnlineParticipationDatasets.spiders.Bonn2017Spider import Bonn2017Spider
+from OnlineParticipationDatasets.spiders.BonnSpider import BonnSpider
 from datetime import datetime
 import re
 import locale
 
 
-class Bonn2019Spider(Bonn2017Spider):
+class Bonn2019Spider(BonnSpider):
     name = "bonn2019"
     start_urls = ['https://www.bonn-macht-mit.de/node/2900']
 
