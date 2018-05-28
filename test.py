8309293aca1f7883c07f055ac47b2d36f58fe5a2
PGrawe <PGrawe@users.noreply.github.com>
2018-05-28 19:13:36 +0200

    Working solution with twisted

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 
-# from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.schedulers.blocking import BlockingScheduler
+# # from apscheduler.schedulers.background import BackgroundScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
 
 time_s_to_key = {
     'us': 'microseconds',
@@ -19,13 +19,18 @@ time_s_to_key = {
 }
 
 
-def call_scrapy(spider):
-    # runner = CrawlerRunner(get_project_settings())
-    # d = runner.crawl(spider)
+def call_scrapy(spider, seconds):
+    runner = CrawlerRunner(get_project_settings())
+    d = runner.crawl(spider)
+    d.addCallback(callLater_wrapper, seconds, call_scrapy, spider, seconds)
     # d.addBoth(lambda _: reactor.stop())
     # # print('Run spider {} at {}'.format(spider, datetime.now()))
     # reactor.run(installSignalHandlers=0) # the script will block here until the crawling is finished
     # # print('{} finished at {}'.format(spider, datetime.now()))
+    return d
+
+def callLater_wrapper(result, *args, **kwargs):
+    return reactor.callLater(*args,*kwargs)
 
 def on_startup(delta=None):
     if not delta:
@@ -45,19 +50,20 @@ def get_envs():
 
 def main():
     configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
-    scheduler = BlockingScheduler()
-    scheduler.add_job(call_scrapy, 'interval', kwargs={'spider': 'bonn2017'}, seconds=60, start_date=on_startup(), id='crawler')
+    # scheduler = BlockingScheduler()
+    # scheduler.add_job(call_scrapy, 'interval', kwargs={'spider': 'bonn2017'}, seconds=60, start_date=on_startup(), id='crawler')
     # scheduler.start() # for backgroundscheduler
     print('Press Ctrl+{0} to exit -- {1}'.format('Break' if os.name == 'nt' else 'C', datetime.now()))
-
-    try:
-        # This is here to simulate application activity (which keeps the main thread alive).
-        # while True:
-        #     time.sleep(10)
-        scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
-        # Not strictly necessary if daemonic mode is enabled but should be done if possible
-        scheduler.shutdown()
+    call_scrapy('bonn2017', 30)
+    reactor.run()
+    # try:
+    #     # This is here to simulate application activity (which keeps the main thread alive).
+    #     # while True:
+    #     #     time.sleep(10)
+    #     scheduler.start()
+    # except (KeyboardInterrupt, SystemExit):
+    #     # Not strictly necessary if daemonic mode is enabled but should be done if possible
+    #     scheduler.shutdown()
 
 if __name__ == '__main__':
     main()
