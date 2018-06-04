c597b520446fc0e46e238a99d7275049cfe79ce0
PGrawe <PGrawe@users.noreply.github.com>
2018-06-04 17:57:59 +0200

    Wait for db in entryfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,16 @@ set -e
 
 source venv/bin/activate
 
+until nc -z -w 5 $CONTENT_HOST $CONTENT_PORT; do
+    sleep 1
+done
+
 if [ "$1" == "run" ]
 then
     exec scrapyd
 elif [ "$1" == "pump" ]
 then
-    exec python test.py
+    exec python crawl_scheduler.py
 fi
 
 exec "$@"
